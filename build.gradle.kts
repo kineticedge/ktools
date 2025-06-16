fda30f0605016b406125b61a85f8ca1b8f4196ad
Neil Buesing <neil@kineticedge.io>
2025-06-15 21:41:06 -0500

    move to Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ subprojects {
     plugins.apply("application")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     dependencies {
